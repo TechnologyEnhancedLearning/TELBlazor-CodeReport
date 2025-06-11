4a7666906586e2096a0252188a567792795fe872
kevwhitt-hee <kevwhitt-hee@users.noreply.github.com>
2025-06-11 14:50:07 +0000

    Deploying to gh-pages from @ TechnologyEnhancedLearning/TELBlazor-CodeReport@d0e284f77873817e0bbe0bdcc802d18c50777145 🚀

--- main.js
+++ main.js
@@ -221,7 +221,7 @@ var assemblies = [
       { "name": "TELBlazor.Components.Core.TELComponentBase", "rp": "TELBlazor.Components_TELComponentBase.html", "cl": 8, "ucl": 0, "cal": 8, "tl": 28, "cb": 0, "tb": 0, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "TELBlazor.Components.OptionalImplementations.Core.DI.DI", "rp": "TELBlazor.Components_DI.html", "cl": 0, "ucl": 2, "cal": 2, "tl": 25, "cb": 0, "tb": 0, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "TELBlazor.Components.ShowCase.Shared.Services.HelperServices.SerilogLogLevelSwitcherService", "rp": "TELBlazor.Components_SerilogLogLevelSwitcherService.html", "cl": 0, "ucl": 99, "cal": 99, "tl": 165, "cb": 0, "tb": 26, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
-      { "name": "TELBlazor.Components.TELBlazorPackageVersion.VersionInfo", "rp": "TELBlazor.Components_VersionInfo.html", "cl": 2, "ucl": 0, "cal": 2, "tl": 2, "cb": 0, "tb": 0, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
+      { "name": "TELBlazor.Components.TELBlazorPackageVersion.VersionInfo", "rp": "TELBlazor.Components_VersionInfo.html", "cl": 2, "ucl": 0, "cal": 2, "tl": 8, "cb": 2, "tb": 4, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
     ]},
 ];
 
